45a1f1d8e305648dfb5f414a0aea2932972e15e9
Lex Bailey <danieljabailey@gmail.com>
2022-04-11 00:14:48 +0100

    refactor done

--- controller/src/cli.rs
+++ controller/src/cli.rs
@@ -68,7 +68,10 @@ fn init_render_data() -> RenderData{
 }
 
 fn draw(gfx: &RenderData, state: &ClientState){
-    let cube = state.cube;
+    let mut cube = state.cube;
+    if state.led_detect_state.active{
+        cube = Cube::new();
+    }
     fn nb (f: &cube::Face, i:usize, tc: &TermCols) -> String{ color_string(i.to_string(), f.subfaces[i].color, &tc) }
     fn bb (f: &cube::Face, i:usize, tc: &TermCols) -> String{ color_string("".to_string(), f.subfaces[i].color, &tc) }
 
@@ -113,9 +116,13 @@ fn draw(gfx: &RenderData, state: &ClientState){
     println!("              ┗━━━━━━━━━━━━━┛");
     println!("              Front ({})", cube::FRONT);
 
-    if state.detect_state.active{
-        println!("Detecting switch input for twist: {}", state.detect_state.twist);
+    if state.input_detect_state.active{
+        println!("Detecting switch input for twist: {}", state.input_detect_state.twist);
         println!("Push the switch between the RED and GREEN LEDs towards the GREEN LED");
+    }
+    if state.led_detect_state.active{
+        println!("Currently detecting LEDs. Use the `map <face_num> <subface_num>` command to map the currently lit LED.");
+        println!("Currently detecting LED number {}", state.led_detect_state.led_num);
     }
 }
 
@@ -193,27 +200,13 @@ fn main() {
                             sender.send(SetState(Cube::new()));
                         }
                         ,"detect leds" => {
-                            //println!("Starting LED detect sequence...");
-                            //println!("Use 'detect next' to move to next LED");
-                            //send_command(&sender, "detect", vec!["leds"]);
-                            //detecting_led = 0;
-                            //detect_led_ui(detecting_led, &sender);
+                            sender.send(DetectLEDs());
+                        }
+                        ,"map undo" => {
+                            sender.send(BacktrackLEDDetect());
                         }
                         ,"detect inputs" => {
-                            //println!("Starting input detect sequence...");
-                            //println!("Use 'detect next' to move to next input");
-                            //send_event(&sender, Event::DetectInputs());
                             sender.send(DetectInputs());
-                        }
-                        ,"detect next" => {
-                            //println!("Next item...");
-                            //detecting_led += 1;
-                            //detect_led_ui(detecting_led, &sender);
-                        }
-                        ,"detect done" => {
-                            //println!("Done detecting, sending new config");
-                            //send_command(&sender, "led_mapping", vec![&cube::serialise_output_map(&led_map)]);
-                            //send_command(&sender, "play", vec![]);
                         }
                         ,"start" => {
                             sender.send(StartGame());
@@ -235,24 +228,23 @@ fn main() {
                                         }
                                     }
                                 }
-                                //,"map" => {
-                                //    if args.len() != 2{
-                                //        println!("map requires two parameters");
-                                //    }
-                                //    else{
-                                //        if let Ok((f, s)) = (||{
-                                //            Result::<(usize, usize), std::num::ParseIntError>::Ok((
-                                //                usize::from_str(args[0])?
-                                //                ,usize::from_str(args[1])?
-                                //            ))
-                                //        })() {
-                                //            led_map[detecting_led as usize] = Output{face:f, subface:s};
-                                //            println!("mapped led {} to (face, subface) = ({}, {})", detecting_led, f, s);
-                                //            detecting_led += 1;
-                                //            detect_led_ui(detecting_led, &sender);
-                                //        }
-                                //    }
-                                //}
+                                ,"map" => {
+                                    if args.len() != 2{
+                                        println!("map requires two parameters");
+                                    }
+                                    else{
+                                        let mut state = state.lock().unwrap();
+                                        if let Ok((f, s)) = (||{
+                                            Result::<(usize, usize), std::num::ParseIntError>::Ok((
+                                                usize::from_str(args[0])?
+                                                ,usize::from_str(args[1])?
+                                            ))
+                                        })() {
+                                            sender.send(MapLED(f, s));
+                                            println!("mapped led {} to (face, subface) = ({}, {})", state.led_detect_state.led_num, f, s);
+                                        }
+                                    }
+                                }
                                 ,_ => {println!("Unknown command: {}",cmd);}
                             }
                         }
@@ -271,8 +263,10 @@ fn main() {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                u_sender.send(UserInput(line));
-                sync_receiver.recv().unwrap();
+                for line in line.lines(){
+                    u_sender.send(UserInput(line.to_string()));
+                    sync_receiver.recv().unwrap();
+                }
             }
             ,Err(ReadlineError::Interrupted) => {
             }
@@ -287,163 +281,5 @@ fn main() {
         }
     }
     rl.save_history(".cube_control_history").unwrap();
-
-/*
-    let mut sender: Option<Sender<Event>> = None;
-    let mut net_thread: Option<JoinHandle<()>> = None;
-    let mut event_thread: Option<JoinHandle<()>> = None;
-
-
-    fn send_event(sender: &Option<Sender<Event>>, ev: Event) {
-        if sender.is_none(){
-            println!("Not connected, please run `connect` command first.");
-            return;
-        }
-        let sender = sender.as_ref().unwrap();
-        let result = sender.send(ev);
-        match result {
-            Ok(_) => {}
-            ,Err(e) => {println!("Failed to send command: {:?}", e);}
-        }
-    }
-
-    fn send_command(sender: &Option<Sender<Event>>, command: &str, args: Vec<&str>){
-        send_event(sender, Event::Command(command.to_string(), args.iter().map(|s|s.to_string()).collect()));
-    }
-
-    draw(&gfx, &data);
-
-    let mut led_map: OutputMap5Faces = [Output{face:0,subface:0}; 45];
-
-    let mut detecting_led: u32 = 0;
-    
-    fn detect_led_ui(led: u32, sender: &Option<Sender<Event>>){
-        if sender.is_none(){
-            println!("not connected, connect and then run 'detect again'");
-            return;
-        }
-        let mut test_state = String::new();
-        for i in 0..54{
-            test_state.push(if i == led {'W'} else {' '});
-        }
-        send_command(sender, "set_state", vec![&test_state]);
-        println!("LED {} is lit. Use 'map <F> <S>' to map this LED to face F and subface S", led);
-    }
-    
-    loop {
-        let command = prompt(&mut data, &mut user_input);
-        match command.as_ref(){
-            "connect" => {
-                if sender.is_some(){
-                    sender = None;
-                    net_thread.unwrap().join();
-                    net_thread = None;
-                    event_thread.unwrap().join();
-                    event_thread = None;
-                }
-                let result = start_service_threads();
-                match result{
-                    Err(e) => {
-                        println!("Error connecting to remote: {:?}", e);
-                    }
-                    Ok((new_net, new_events, new_sender)) => {
-                        sender = Some(new_sender);
-                        net_thread = Some(new_net);
-                        event_thread = Some(new_events);
-                        println!("Connected");
-                        send_command(&sender, "play", vec![]);
-                    }
-                }
-            }
-            ,"show" => {
-                draw(&gfx, &data);
-            }
-            ,"solved" => {
-                data.cube = cube_model::Cube::new();
-                send_command(&sender, "set_state", vec![&data.cube.serialise()]);
-                draw(&gfx, &data);
-            }
-            ,"detect leds" => {
-                println!("Starting LED detect sequence...");
-                println!("Use 'detect next' to move to next LED");
-                send_command(&sender, "detect", vec!["leds"]);
-                detecting_led = 0;
-                detect_led_ui(detecting_led, &sender);
-            }
-            ,"detect inputs" => {
-                println!("Starting input detect sequence...");
-                println!("Use 'detect next' to move to next input");
-                send_event(&sender, Event::DetectInputs());
-            }
-            ,"detect next" => {
-                println!("Next item...");
-                detecting_led += 1;
-                detect_led_ui(detecting_led, &sender);
-            }
-            ,"detect done" => {
-                println!("Done detecting, sending new config");
-                send_command(&sender, "led_mapping", vec![&cube::serialise_output_map(&led_map)]);
-                send_command(&sender, "play", vec![]);
-            }
-            ,"start" => {
-                data.cube = cube_model::Cube::new();
-                let mut last_twist = Twist::from_string("F").unwrap();
-                let mut twist = Twist::from_string("F").unwrap();
-                let mut rng = rand::rngs::OsRng;
-                // A very naive scramble algorithm
-                for i in 0..30{
-                    while twist == last_twist{
-                        twist = Twist{
-                            face: rng.gen_range(0..6)
-                            ,reverse: rng.gen_bool(0.5)
-                        }
-                    }
-                    last_twist = twist;
-                    data.cube.twist(twist);
-                }
-                send_command(&sender, "set_state", vec![&data.cube.serialise()]);
-                send_command(&sender, "play", vec![]);
-            }
-            ,"" => {}
-            ,cmd => {
-                let mut parts = cmd.split(' ');
-                let name = parts.next().unwrap();
-                let args_str = &cmd[name.len()..cmd.len()];
-                let args = parts.collect::<Vec<&str>>();
-                match name.as_ref(){
-                    "twist" => {
-                        match data.cube.twists(args_str){
-                            Err(msg) => {println!("Error: {}", msg)}
-                            ,Ok(_) => {
-                                send_command(&sender, "set_state", vec![&data.cube.serialise()]);
-                                draw(&gfx, &data);
-                                println!("Done twists.");
-                            }
-                        }
-                    }
-                    ,"map" => {
-                        if args.len() != 2{
-                            println!("map requires two parameters");
-                        }
-                        else{
-                            if let Ok((f, s)) = (||{
-                                Result::<(usize, usize), std::num::ParseIntError>::Ok((
-                                    usize::from_str(args[0])?
-                                    ,usize::from_str(args[1])?
-                                ))
-                            })() {
-                                led_map[detecting_led as usize] = Output{face:f, subface:s};
-                                println!("mapped led {} to (face, subface) = ({}, {})", detecting_led, f, s);
-                                detecting_led += 1;
-                                detect_led_ui(detecting_led, &sender);
-                            }
-                        }
-                    }
-                    ,_ => {println!("Unknown command: {}",cmd);}
-                }
-            }
-        }
-    }
-*/
 
 }
--- controller/src/client.rs
+++ controller/src/client.rs
@@ -16,7 +16,7 @@ use std::collections::HashSet;
 
 use plain_authentic_commands::{MessageHandler, ParseStatus};
 
-pub struct DetectState {
+pub struct InputDetectState {
     pub twist: usize
     ,pub cur_sample: usize
     ,pub samples: [Option<u32>;5]
@@ -31,10 +31,10 @@ enum DetectMessage {
     ,Mapping(String)
 }
 
-impl DetectState{
+impl InputDetectState{
 
     fn new() -> Self{
-        DetectState{
+        InputDetectState{
             twist: 0
             ,cur_sample: 0
             ,samples: [None;5]
@@ -138,16 +138,71 @@ impl DetectState{
     }
 }
 
+pub struct LEDDetectState {
+    pub active: bool
+    ,pub led_num: usize
+    ,pub led_map: OutputMap5Faces
+}
+
+impl LEDDetectState {
+    fn new() -> LEDDetectState {
+        LEDDetectState{
+            active: false
+            ,led_num: 0
+            ,led_map: [Output{face:0,subface:0}; 45]
+        }
+    }
+
+    fn map(&mut self, f: usize, s: usize) {
+        if self.led_num < self.led_map.len() {
+            self.led_map[self.led_num] = Output{face:f, subface:s};
+            self.led_num += 1;
+        }
+        else{
+            self.active = false;
+        }
+    }
+
+    fn backtrack(&mut self) {
+        if self.led_num > 0{
+            self.led_num -= 1;
+        }
+    }
+
+    fn get_mapping(&self) -> String{
+        cube_model::serialise_output_map(&self.led_map)
+    }
+
+    fn is_done(&self) -> bool{
+        self.led_num >= self.led_map.len()
+    }
+
+    fn get_state(&self) -> String{
+        let mut test_state = String::new();
+        for i in 0..54{
+            test_state.push(if i == self.led_num {'W'} else {' '});
+        }
+        test_state
+    }
+
+    fn activate(&mut self){
+        self.active = true;
+    }
+
+}
+
 pub struct ClientState {
     pub cube: Cube
-    ,pub detect_state: DetectState
+    ,pub input_detect_state: InputDetectState
+    ,pub led_detect_state: LEDDetectState
 }
 
 impl ClientState {
     fn new() -> ClientState{
         ClientState {
             cube: Cube::new()
-            ,detect_state: DetectState::new()
+            ,input_detect_state: InputDetectState::new()
+            ,led_detect_state: LEDDetectState::new()
         }
     }
 }
@@ -186,6 +241,8 @@ pub enum FromGUI {
     ,StartGame()
     ,SetState(Cube)
     ,SyncState()
+    ,MapLED(usize, usize)
+    ,BacktrackLEDDetect()
 }
 
 #[derive(Debug)]
@@ -248,106 +305,6 @@ impl<T: Read + Write, C: Connector<Stream=T>> Messenger<T, C>{
         Ok(())
     }
 }
-
-/*
-fn start_service_threads() -> io::Result<(JoinHandle<()>, JoinHandle<()>, Sender<Event>)>{
-
-    let (sender, receiver) = channel();
-
-    // Split the sender into two
-    let CLI_sender = sender.clone();
-    let service_sender = sender;
-
-    let mut msg = TcpMessenger::new(b"secret".to_vec(), "localhost:9876");
-    msg.connect()?;
-
-    let mut reader = msg.stream.as_ref().unwrap().try_clone()?;
-
-    let net_thread = thread::spawn(move||{
-        handle_responses(&mut reader, service_sender);
-    });
-    
-    let event_thread = thread::spawn(move||{
-        let mut command_queue: VecDeque<(String, Vec<String>)> = VecDeque::new();
-        let mut got_challenge = false;
-
-        fn send_events(got_challenge: &mut bool, command_queue: &mut VecDeque<(String, Vec<String>)>, msg: &mut TcpMessenger){
-            if *got_challenge {
-                if let Some((command, args)) = command_queue.pop_front() {
-                    *got_challenge = false;
-                    //println!("Send command: {}, {:?}", command, args);
-                    let args = args.iter().map(|a|a.as_ref()).collect();
-                    msg.send_command(&command, &args);
-                }
-            }
-        }
-
-        let mut detect_state = DetectState::new();
-
-        for event in receiver.iter(){
-            match event {
-                Event::Response(s) => {
-                    match msg.handler.parse_response(&s) {
-                        ParseStatus::Success(response, args) => {
-                            match response.as_ref() {
-                                "challenge" => {
-                                    got_challenge = true;
-                                }   
-                                ,"input" => {
-                                    println!("user applied input: {}", args[0]);
-                                    if let Ok(input) = u32::from_str(&args[0]){
-                                        use DetectMessage::*;
-                                        match detect_state.sample_input(input){
-                                            Nothing() => {
-                                                // do nothing
-                                            }
-                                            ,TestState(test_state) => {
-                                                command_queue.push_back(("set_state".to_string(), vec![test_state]));
-                                            }
-                                            ,Mapping(mapping) => {
-                                                command_queue.push_back(("input_mapping".to_string(), vec![mapping]));
-                                            }
-                                        }
-                                    }
-                                    else {
-                                        println!("Not a valid number: {}", args[0]);
-                                    }
-                                }
-                                ,"twist" => {
-                                    println!("Twist: {}", args[0]);
-                                }
-                                ,r=>{
-                                    eprintln!("TODO handle response: {}", r);
-                                }   
-                            };  
-                        }   
-                        ,ParseStatus::BadClient() => {
-                            eprintln!("Reply appears malformed");
-                            return;
-                        }
-                        ,ParseStatus::Unauthorised() => {
-                            eprintln!("Reply appears inauthentic");
-                            return;
-                        }
-                    };
-                }
-                ,Event::Command(command, args) => {
-                    command_queue.push_back((command, args));
-                }
-                ,Event::DetectInputs() => {
-                    command_queue.push_back(("detect".to_string(), vec!["inputs".to_string()]));
-                    detect_state = DetectState::new();
-                    detect_state.activate();
-                    let test_state = detect_state.ui();
-                    command_queue.push_back(("set_state".to_string(), vec![test_state]));
-                }
-            }
-            send_events(&mut got_challenge, &mut command_queue, &mut msg);
-        }
-    });
-    Ok((net_thread, event_thread, CLI_sender))
-}
-*/
 
 pub fn start_client() -> (Arc<Mutex<ClientState>>, Sender<FromGUI>, Receiver<ToGUI>, JoinHandle<()>) {
     let state = Arc::new(Mutex::new(ClientState::new()));
@@ -480,7 +437,7 @@ pub fn start_client() -> (Arc<Mutex<ClientState>>, Sender<FromGUI>, Receiver<ToG
                                         if let Ok(input) = u32::from_str(&args[0]){
                                             use DetectMessage::*;
                                             let mut state = state.lock().unwrap();
-                                            match state.detect_state.sample_input(input){
+                                            match state.input_detect_state.sample_input(input){
                                                 Nothing() => {
                                                     // do nothing
                                                 }
@@ -508,12 +465,13 @@ pub fn start_client() -> (Arc<Mutex<ClientState>>, Sender<FromGUI>, Receiver<ToG
                                             to_gui_sender.send(ToGUI::StateUpdate());
                                         }
                                     }
-                                    ,"state" => {
-                                        if args.len() >= 1{
-                                            let mut state = state.lock().unwrap();
-                                            state.cube.deserialise(&args[0]);
-                                        }
-                                    }
+                                    // TODO make this work (sync state from service)
+                                    //,"state" => {
+                                    //    if args.len() >= 1{
+                                    //        let mut state = state.lock().unwrap();
+                                    //        state.cube.deserialise(&args[0]);
+                                    //    }
+                                    //}
                                     ,"solved" => {
                                         let mut state = state.lock().unwrap();
                                         state.cube = Cube::new();
@@ -558,13 +516,37 @@ pub fn start_client() -> (Arc<Mutex<ClientState>>, Sender<FromGUI>, Receiver<ToG
                             msg = Some(m);
                         }
 
-                        ,DetectLEDs() => {println!("TODO leds detect");}
+                        ,DetectLEDs() => {
+                            let mut state = state.lock().unwrap();
+                            state.led_detect_state = LEDDetectState::new();
+                            state.led_detect_state.activate();
+                            let test_state = state.led_detect_state.get_state();
+                            command_queue.push_back(("detect".to_string(), vec!["leds".to_string()]));
+                            command_queue.push_back(("set_state".to_string(), vec![test_state]));
+                            to_gui_sender.send(ToGUI::StateUpdate());
+                        }
+                        ,MapLED(f, s) => {
+                            let mut state = state.lock().unwrap();
+                            if state.led_detect_state.active{
+                                state.led_detect_state.map(f,s);
+                            }
+                            let test_state = state.led_detect_state.get_state();
+                            command_queue.push_back(("set_state".to_string(), vec![test_state]));
+                            to_gui_sender.send(ToGUI::StateUpdate());
+                        }
+                        ,BacktrackLEDDetect() => {
+                            let mut state = state.lock().unwrap();
+                            state.led_detect_state.backtrack();
+                            let test_state = state.led_detect_state.get_state();
+                            command_queue.push_back(("set_state".to_string(), vec![test_state]));
+                            to_gui_sender.send(ToGUI::StateUpdate());
+                        }
                         ,DetectInputs() => {
                             let mut state = state.lock().unwrap();
                             command_queue.push_back(("detect".to_string(), vec!["inputs".to_string()]));
-                            state.detect_state = DetectState::new();
-                            state.detect_state.activate();
-                            let test_state = state.detect_state.get_led_state();
+                            state.input_detect_state = InputDetectState::new();
+                            state.input_detect_state.activate();
+                            let test_state = state.input_detect_state.get_led_state();
                             state.cube.deserialise(&test_state);
                             command_queue.push_back(("set_state".to_string(), vec![test_state]));
                             to_gui_sender.send(ToGUI::StateUpdate());
@@ -588,6 +570,7 @@ pub fn start_client() -> (Arc<Mutex<ClientState>>, Sender<FromGUI>, Receiver<ToG
                             }
                             command_queue.push_back(("set_state".to_string(), vec![state.cube.serialise()]));
                             command_queue.push_back(("play".to_string(), vec![]));
+                            to_gui_sender.send(ToGUI::StateUpdate());
                         }
                         ,SetState(cube) => {
                             let mut state = state.lock().unwrap();
